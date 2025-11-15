d48bcaabb736c29381d7aa4b73edba17785b2d17
ritikj0536 <124515293+ritikj0536@users.noreply.github.com>
2025-11-15 15:27:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
-  // Call Person constructor
   Person.call(this, name, age);
-
   this.jobTitle = jobTitle;
 }
 
